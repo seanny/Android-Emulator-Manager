45f18d206112c663c80694d0cdb48c7d08a94d0f
Sean McElholm <seanmcelholm@gmail.com>
2019-09-05 16:38:19 +0100

    Initial Commit

---
+++ Android Emulator Manager/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Android Emulator Manager
+//
+//  Created by Sean McElholm on 05/09/2019.
+//  Copyright © 2019 Sean McElholm. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Android Emulator Manager/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Android Emulator Manager
+//
+//  Created by Sean McElholm on 05/09/2019.
+//  Copyright © 2019 Sean McElholm. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
